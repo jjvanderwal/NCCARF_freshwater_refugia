bb6c519f0e540ba9a2fb0feee1d6380a7a2e5d5a
Jeremy VanDerWal <jjvanderwal@gmail.com>
2012-10-25 21:05:22 +1000

    optimized the accum function for speed

--- hydrology/dev/accumulate_attributes.R
+++ hydrology/dev/accumulate_attributes.R
@@ -26,38 +26,37 @@ gg = decompose.graph(g,"weak") #break the full graph into 10000 + subgraphs
 
 ### do the accumulation
 #function to accumulate info in each subgraph in a full graph
-gt = gg[[827]]
-degree(gt,mode="out")
-
+gt=gg[[975]]
 accum = function(gt) { 
 	require(igraph)
 	out=NULL #define the output
 	while(length(E(gt))>0) { #loop until all edges are dealt with
 		vois = which(degree(gt,mode="in")==0) #get index of the headwater vertices
-		for (voi in vois) { #cycle through the vertices
-			eois = incident(gt,V(gt)[voi],"out") #get an index of the output edges from that vertex
-			for (eoi in eois) {
-				e_tmp = E(gt)[eoi] #this is the edge being worked with
-				runoff=e_tmp$LocalRunoff #get the runoff
-				out = rbind(out,data.frame(HydroID=e_tmp$HydroID,runoff=runoff)) #append the current data
-				v2 = get.edge(gt,e_tmp)[2] #get the index of the "to_vertex"
-				next_edges = incident(gt,V(gt)[v2],"out") #get an index of the output edges from that vertex
-				for (next_edge in next_edges) { #cycle through the next edges and add the upstream flow
-					E(gt)$LocalRunoff[next_edge] = E(gt)$LocalRunoff[next_edge] + (E(gt)$BiProp[next_edge] * runoff) #add the appropriate proportion of upstream runoff
-				}
-			}
+		v.from.to = NULL; for (ii in neighborhood(gt,1,V(gt)[vois],"out")) { #get the index of the from & to nodes for each edge
+			if (length(ii)>1) v.from.to = rbind(v.from.to,data.frame(from=ii[1],to=ii[2:length(ii)]))
 		}
-		gt = delete.vertices(gt, V(gt)[vois])#remove the vois
+		eois = get.edge.ids(gt,t(cbind(V(gt)[v.from.to[,1]],V(gt)[v.from.to[,2]]))) #get an index of the output edges from that vertex
+		out = rbind(out,data.frame(HydroID=E(gt)$HydroID[eois],runoff=E(gt)$LocalRunoff[eois])) #store the runoff for the current edges
+		
+		tt = neighborhood(gt,1,V(gt)[v.from.to[,2]],"out") #get the next down verticies from the current edges
+		next_edge = NULL; for (ii in 1:length(eois)) { #get an index of the next down vertices and aggregate list to dataframe
+			if (length(tt[[ii]])>1) next_edge = rbind(next_edge,data.frame(e.from=eois[ii],from=tt[[ii]][1],to=tt[[ii]][2:length(tt[[ii]])]))
+		}
+		if (!is.null(next_edge)) { #only do this if there is something down stream
+			next_edge$e.next = get.edge.ids(gt,t(cbind(V(gt)[next_edge[,2]],V(gt)[next_edge[,3]]))) #get an index of the next down edges
+			E(gt)$LocalRunoff[next_edge$e.next] = E(gt)$LocalRunoff[next_edge$e.next] + (E(gt)$BiProp[next_edge$e.from] * E(gt)$LocalRunoff[next_edge$e.from])#append appropriate runoff to next down edges using proportions
+		}
+		gt = delete.vertices(gt, V(gt)[vois]) #remove the vois
 	}
 	return(out)
 }
 
-ncore=10
+###do the actual accumulation
+ncore=5 #this number of cores seems most appropriate
 cl <- makeCluster(getOption("cl.cores", ncore))#define the cluster for running the analysis
-	print(system.time({
-		tout = parLapplyLB(cl,gg[1:100],accum)
-	}))
+	print(system.time({ tout = parLapplyLB(cl,gg,accum) }))
 stopCluster(cl) #stop the cluster for analysis
 
-out = NULL; for (ii in 1:length(tout)) {cat('.');out = rbind(out,tout[[ii]])}
+out = NULL; for (ii in 1:length(tout)) {cat('.');out = rbind(out,tout[[ii]])} #aggregate the list into a single matrix
 db2 = merge(db,out)
+
