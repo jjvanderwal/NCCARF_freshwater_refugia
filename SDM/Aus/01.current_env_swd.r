4fac6950bc319c92cea6764733e009a323833b0e
Jeremy VanDerWal <jjvanderwal@gmail.com>
2013-01-25 14:18:27 +1000

    optimized and corrected data prep for SDMs

--- SDM/Aus/01.current_env_swd.r
+++ SDM/Aus/01.current_env_swd.r
@@ -10,41 +10,22 @@ bioclim=read.csv("/home/jc246980/Climate/5km/Future/Bioclim_reach/Current_biocli
 dryseason.dir="/home/jc246980/DrySeason/DrySeason_reach/"
 VOIS=c("num.month", "total.severity", "max.clust.length","clust.severity", "month.max.clust")
 
-### Create annual means and fill in accumulated flow gaps
+out = bioclim; out$lat = out$lon = out$SegmentNo; out = out[,c('SegmentNo','lat','lon',colnames(bioclim)[-1])] #define the output data replicating segment number as lat and lon
 
+### Create annual means and fill in accumulated flow gaps
 hydro$MeanAnnual=rowSums(hydro[,2:13])
 Runoff$MeanAnnual=rowSums(Runoff[,2:13])
 hydro_extra=Runoff[which(!(Runoff$SegmentNo %in% hydro$SegmentNo)),]   
 HYDRO=hydro[,c(1,14)]
 HYDRO_EXTRA=hydro_extra[, c(1,14)]
-HYDROLOGY=rbind(HYDRO,HYDRO_EXTRA)
+HYDROLOGY=rbind(HYDRO,HYDRO_EXTRA); colnames(HYDROLOGY)[2]='Flow_accum_annual'
 
 ### Create current environmental data file
+for(voi in VOIS) { cat(voi,'\n') 	
+	tdata=read.csv(paste(dryseason.dir,"Current_",voi,".csv", sep='')) 			# load data for each varable
+	if (voi==VOIS[1]) Enviro_dat=tdata else Enviro_dat=merge(Enviro_dat,tdata)
+}
 
-	for(voi in VOIS) { cat(voi,'\n') 	
-			
-		tdata=read.csv(paste(dryseason.dir,"Current_",voi,".csv", sep='')) 			# load data for each varable
-		
-		if (voi==VOIS[1]) Enviro_dat=tdata else Enviro_dat=cbind(Enviro_dat,tdata[,2])
+out = merge(out,Enviro_dat); out = merge(out,HYDROLOGY) #fully define the output
+current = out; save(current,file='/home/jc165798/working/NARP_FW_SDM/current_enviro_data.Rdata') #write out the data
 
-	}
-
-Enviro_dat=merge(bioclim,Enviro_dat, by='SegmentNo',all.x=TRUE)	
-
-Enviro_dat=merge(Enviro_dat, HYDROLOGY, by="SegmentNo", all.x=TRUE)
-
-### Create a fake lat/lon from SegmentNo - maxent demands lat/lon
-	Enviro_dat=cbind(Enviro_dat[,1],Enviro_dat)
-
-### LABEL the future env layers with the exact same column names as current layers.  IMPORTANT!
-	tt=c('lat','long',paste('bioclim_',sprintf('%02i',c(1:19)),sep=""),"num.month", "total.severity", "max.clust.length","clust.severity", "month.max.clust", 'MeanAnnual')
-	
-	colnames(Enviro_dat)=tt
-
-out.dir ="/home/jc148322/NARPfreshwater/SDM/Env_layers/"
-write.csv(Enviro_dat,paste(out.dir,"current.csv",sep=''),row.names=F)
-
-# also save it as Rdata
-setwd(out.dir)
-current=Enviro_dat
-save(current,file='current.Rdata')
--- SDM/Aus/02.batch_env_swd_prep.r
+++ SDM/Aus/02.batch_env_swd_prep.r
@@ -5,24 +5,20 @@ futdir = "/home/jc165798/Climate/CIAS/Australia/5km/monthly_csv/"
 
 ESs = list.files(futdir, pattern="RCP") # list the emission scenarios
 GCMs = list.files(paste(futdir,ESs[1],sep=''))	# get a list of GCMs
-YEAR=seq(2015, 2085, 10)	
 
-sh.dir='/home/jc148322/scripts/NARP_freshwater/SDM/prep_data.sh/'; setwd(sh.dir)
+sh.dir='/home/jc165798/tmp_pbs/'; setwd(sh.dir)
 for (es in ESs) {
 	for (gcm in GCMs) {			
-		for (yy in YEAR) {
-			zz = file(paste('02.',es,'.',gcm,'.',yy,'.prep_data.sh',sep=''),'w')
-			 cat('#!/bin/bash\n',file=zz)
-			 cat('cd $PBS_O_WORKDIR\n',file=zz)
-			 cat('source /etc/profile.d/modules.sh\n',file=zz)
-			 cat('module load R-2.15.1\n',file=zz)
-			 cat("R CMD BATCH --no-save --no-restore '--args es=\"",es,"\" gcm=\"",gcm,"\" yy=\"",yy,"\" ' /home/jc148322/scripts/NARP_freshwater/SDM/02.run_env_swd_prep.r 02.",es,'.',gcm,'.',yy,'.Rout \n',sep='',file=zz) #run the R script in the background
+		zz = file(paste(es,'.',gcm,'.prep_data.sh',sep=''),'w')
+			cat('#!/bin/bash\n',file=zz)
+			cat('cd $PBS_O_WORKDIR\n',file=zz)
+			cat('source /etc/profile.d/modules.sh\n',file=zz)
+			cat('module load R-2.15.1\n',file=zz)
+			cat("R CMD BATCH --no-save --no-restore '--args es=\"",es,"\" gcm=\"",gcm,"\" ' /home/jc165798/SCRIPTS/git_code/NCCARF_freshwater_refugia/SDM/Aus/02.run_env_swd_prep.r ",es,'.',gcm,'.Rout \n',sep='',file=zz) #run the R script in the background
 		close(zz) 
 
 		##submit the script
-		system(paste('qsub -l nodes=1:ppn=4 02.',es,'.',gcm,'.',yy,'.prep_data.sh',sep=''))
-
-		}
+		system(paste('qsub -m n -l nodes=1:ppn=5 ',es,'.',gcm,'.prep_data.sh',sep=''))
 	}
 }
 	
